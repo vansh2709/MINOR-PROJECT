545c2109a2b8ea0d4ecb4aa489adeafbf77c35d3
Arindam Chandra Pathak <pathakarindamchandra@gmail.com>
2025-12-06 21:29:44 +0530

    Add class 'links' to footer anchor tags

--- scheduler/src/components/Footer.jsx
+++ scheduler/src/components/Footer.jsx
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
     <footer className="footer">
-      <a href="#">T's & C's</a>
-      <a href="#">Contact</a>
-      <a href="#">Exit</a>
+      <a className="links" href="#">T's & C's</a>
+      <a className="links" href="#">Contact</a>
+      <a className="links" href="#">Exit</a>
     </footer>
   );
 }
