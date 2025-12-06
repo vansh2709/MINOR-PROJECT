0ded575a6ed044271a51180d12c9859e2d276011
Arindam Chandra Pathak <pathakarindamchandra@gmail.com>
2025-12-06 21:29:23 +0530

    Refactor Header component for styling updates

--- scheduler/src/components/Header.jsx
+++ scheduler/src/components/Header.jsx
@@ -2,10 +2,10 @@ export default function Header() {
   return (
     <header>
       <nav className="header">
-        <h1>ScheduGrid</h1>
-        <div>
-          <a href="#">About ScheduGrid</a>
-          <a href="#">Contact Admin</a>
+        <h1 className="Title">ScheduGrid</h1>
+        <div className="header-links">
+          <a className="links" href="#">About ScheduGrid</a>
+          <a className="links" href="#">Contact Admin</a>
         </div>
       </nav>
     </header>
