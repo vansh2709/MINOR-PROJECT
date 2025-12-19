6d15c8c2c0f4a70064a26c778efa338a3c5377a2
Rahul Verma <rahulverma.1.2005@gmail.com>
2025-12-20 00:59:39 +0530

    using .env

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,8 +1,13 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./fcm-service-account.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+  });
+}
 
 module.exports = admin;
