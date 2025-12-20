41e5d4f2e51ad82934c2dedb945fb9797cc232e3
Rahul Verma <rahulverma.1.2005@gmail.com>
2025-12-20 08:35:12 +0530

    correcting .env

--- scheduler/src/services/states.jsx
+++ scheduler/src/services/states.jsx
@@ -215,7 +215,7 @@ export const GlobalProvider = ({ children }) => {
         // load announcement
         if (userData?.role === "Teacher") return;
         loadAnnouncements();
-    }, [userData, loadAnnouncements, SubscribePushNotification])
+    }, [userData])
 
 
     const exports = {
